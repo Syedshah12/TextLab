4b37a5abd3f95df494a91c4d9958d29c2e1af148
Syedshah12 <105164745+Syedshah12@users.noreply.github.com>
2023-07-03 13:47:31 +0500

    elimintaing routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
-import About from './components/About';
+// import About from './components/About';
 import TextArea from './components/TextArea';
 import React,{useState} from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [mode, setMode] = useState("light")
@@ -31,7 +31,7 @@ function App() {
   }
 
   return (
-    <Router>
+  
     <>
 
       <Navbar title="TextLab" showAlert={showAlert} alert={alert} about="aboutApp" mode={mode} toggleMode={toggleMode} />
@@ -40,13 +40,12 @@ function App() {
         <h1 className={`mx-2 text-${mode==="light"?"black":"white"}`}>Text<span style={{color:"red"}}>Lab</span> <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-pen" viewBox="0 0 16 16">
           <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
         </svg></span></h1>
-        <Routes>
-          <Route exact path="/" element={<TextArea showAlert={showAlert} mode={mode} toggleMode={toggleMode} />}/>
-          <Route exact path="/about" element={ <About/>}/>
-        </Routes>
+        <TextArea showAlert={showAlert} mode={mode} toggleMode={toggleMode} />
+           {/* <About/> */}
+       
       </div>
     </>
-    </Router>
+
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,20 @@
 import React from "react";
 import PropTypes from 'prop-types'
-import {  Link } from "react-router-dom";
+// import {  Link } from "react-router-dom";
 
 function Navbar(props) {
  
   return (
     <nav className={`navbar navbar-expand-lg bg-${props.mode} py-2`}>
       <div className="container-fluid">
-        <Link className={`navbar-brand heading text-${props.mode==="light"?"black":"white"}`} to="/">{props.title}</Link>
+        <a className={`navbar-brand heading text-${props.mode==="light"?"black":"white"}`} href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className={`nav-link active text-${props.mode==="light"?"black":"white"}`} aria-current="page" to="/about">{props.about}</Link>
+              <a className={`nav-link active text-${props.mode==="light"?"black":"white"}`} aria-current="page" href="/">{props.about}</a>
             </li>
          
 
